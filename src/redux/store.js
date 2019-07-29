4b6c1bff40e3ec4c5b557f5195f2e2eda1397e78
Ahmed Afifi <ahmed.mohsem.afifi@gmail.com>
2019-07-30 02:53:32 +0400

    removing redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,12 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import { persistStore } from 'redux-persist';
 
-const middlewares = [logger];
+const middlewares = [];
+
+// add the middleware and show logger logs only in development mode
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
